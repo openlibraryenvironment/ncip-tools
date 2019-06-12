99403e9a9362646fab4554053c4d3c2dd603631b
Ian Ibbotson <ianibbo@googlemail.com>
2019-06-12 11:07:04 +0100

    Added integration test

---
+++ src/integration-test/groovy/org/olf/ncip/TestClient202.groovy
@@ -0,0 +1,22 @@
+package org.olf.ncip
+
+import spock.lang.Specification
+import spock.lang.Unroll
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.junit.runner.RunWith;
+import org.junit.Test;
+
+/**
+ */
+class TestClient202  extends Specification {
+
+  final static Logger logger = LoggerFactory.getLogger(TestClient202.class);
+
+  @Test
+  public void testLookupBorrower() {
+    assert 1==1
+  }
+
+}
+
--- src/main/groovy/org/olf/ncip/client/BaseClient.groovy
+++ src/main/groovy/org/olf/ncip/client/BaseClient.groovy
@@ -1,7 +1,25 @@
 package org.olf.ncip.client;
 
-public class BaseClient implements  NcipClient {
-  
+import groovyx.net.http.HTTPBuilder
+import groovyx.net.http.Method
+import groovyx.net.http.ContentType
+
+public abstract class BaseClient implements  NcipClient {
+
+  private String address;
+
+  /**
+   *  Construct a new NCIP client given an address of the form hostname:port
+   */
+  public BaseClient(String addres) {
+    this.address = address;
+  }
+
+  HTTPBuilder getSession() {
+    HTTPBuilder http = new HTTPBuilder(address)
+    return http;
+  }
+
   abstract Map lookupUser();
   abstract Map lookupItem();
   abstract Map createUser();

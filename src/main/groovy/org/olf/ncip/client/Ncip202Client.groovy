98ef7309fb97ab8e8ed62486690e93fb7e47958f
Ian Ibbotson <ianibbo@gmail.com>
2019-07-02 13:47:31 +0100

    Added string based address constructor to Ncip202Client

--- src/main/groovy/org/olf/ncip/client/Ncip202Client.groovy
+++ src/main/groovy/org/olf/ncip/client/Ncip202Client.groovy
@@ -1,6 +1,10 @@
 package org.olf.ncip.client;
 
 public class Ncip202Client extends  BaseClient {
+
+  public Ncip202Client(String address) {
+    super(address);
+  }
   
   Map lookupUser() {
     return [:]
